460c0608526d9bcfdbf5a90d409ffc5c0d6eaad2
Whitney <whitney.purdum@gmail.com>
2017-03-19 21:53:34 -0400

    created user alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,15 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+	window.alert("You found a match!");
+} else {
+	window.alert("Sorry, try again");
+};
+
+if (cardOne === cardTwo) {
+	window.alert("You found a match!");
+} else {
+	window.alert("Sorry, try again");
+};
